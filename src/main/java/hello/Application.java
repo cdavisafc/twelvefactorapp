bd26c00d3354d166fdd27af1294562931ecae8a9
Cornelia Davis <cornelia@corneliadavis.com>
2015-09-08 20:23:10 -0700

    Explicitly delcare and isolate dependencies

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,32 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+//import javax.servlet.ServletException;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+}
