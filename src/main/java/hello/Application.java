c3ce3861367c1462215bfa2d8dd3ad5626cebc5d
krujos <jkruck@pivotal.io>
2015-09-10 21:02:32 -0500

    add application logging levels

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,16 +1,19 @@
 package hello;
 
-import java.util.Arrays;
-
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
 //import javax.servlet.ServletException;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+    private static Logger log = Logger.getLogger(Application.class);
 	
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -20,12 +23,12 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        log.info("Let's inspect the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-            System.out.println(beanName);
+            log.info(beanName);
         }
     }
 
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,16 @@
 package hello;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+    private static Logger log = Logger.getLogger(HelloController.class);
 
     @RequestMapping("/")
     public String index() {
+        log.debug("I intend to greet someone");
         return "Greetings from Spring Boot!";
     }
 
