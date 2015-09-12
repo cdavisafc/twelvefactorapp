7334062500a313cc73e571c502dfef3dd6b14b5f
Cornelia Davis <cornelia@corneliadavis.com>
2015-09-12 05:48:19 -0700

    Store config in the environment

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,12 +3,22 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
 @RestController
-public class HelloController {
+public class HelloController implements EnvironmentAware {
+	
+	private String name;
+	
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.name = environment.getProperty("who");
+	}
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings " + name + " from Spring Boot!";
     }
 
 }
