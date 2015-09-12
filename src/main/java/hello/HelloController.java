e7b64f59186370a8840b239c079fec8a416fb4ca
Cornelia Davis <cornelia@corneliadavis.com>
2015-09-12 06:39:34 -0700

    merge f3 branch

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,14 +4,24 @@ import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
 @RestController
-public class HelloController {
+public class HelloController implements EnvironmentAware {
     private static Logger log = Logger.getLogger(HelloController.class);
+	
+	private String name;
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.name = environment.getProperty("who");
+	}
 
     @RequestMapping("/")
     public String index() {
         log.debug("I intend to greet someone");
-        return "Greetings from Spring Boot!";
+        return "Greetings " + name + " from Spring Boot!";
     }
 
 }
