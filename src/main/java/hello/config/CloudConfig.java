0cde865f653b8b149d98d2a6f459dc326d1e7107
krujos <jkruck@pivotal.io>
2015-09-12 20:00:46 -0500

    Use spring cloud connector

---
+++ src/main/java/hello/config/CloudConfig.java
@@ -0,0 +1,9 @@
+package hello.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.config.java.*;
+
+@Configuration
+@ServiceScan
+class CloudConfig {
+}
